0adda628397d1387b1578d8f8aa4cee185b95e84
amankaushik <amankaushik104@gmail.com>
2016-10-22 22:52:12 +0530

    change limit on number of articles received, remove refresh option

--- notifyProject/notify/views.py
+++ notifyProject/notify/views.py
@@ -2,8 +2,6 @@
 Views
 '''
 
-import json
-import notify.utils as utils
 from notify.models import NewsItem
 from notify.serializer import NewsItemSerializer
 from notify.implementation import HackerNewsAPIImpl
@@ -17,15 +15,8 @@ class NewsList(generics.ListCreateAPIView):
     serializer_class = NewsItemSerializer
     http_method_names = ['get']
 
-
 class NewsTop(generics.ListCreateAPIView):
     #permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = NewsItem.objects.filter(isNew=True)
     serializer_class = NewsItemSerializer
     http_method_names = ['get']
-
-
-def refreshNewsData(request):
-    if request.method == 'GET':
-        utils.refresh_news_data()
-        return HttpResponse(json.dumps({'result': 'success'}), content_type='application/json')
