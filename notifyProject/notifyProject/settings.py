0ed320e2e98e002e995e8791edb31eea2ad8021b
amankaushik <amankaushik104@gmail.com>
2016-10-22 23:53:47 +0530

    cors config

--- notifyProject/notifyProject/settings.py
+++ notifyProject/notifyProject/settings.py
@@ -58,8 +58,8 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'notifyProject.urls'
-# CORS_URLS_REGEX = r'^/api/.*$'
-#CORS_ORIGIN_ALLOW_ALL = False
+CORS_URLS_REGEX = r'^/api/.*$'
+CORS_ORIGIN_ALLOW_ALL = True
 #CORS_ALLOW_CREDENTIALS = True
 # CORS_ORIGIN_WHITELIST = ('localhost:3000')
 
